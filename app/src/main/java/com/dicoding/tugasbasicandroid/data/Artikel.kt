2b552f8c4edd07ead998203e27831bd66306b891
Rizky Agung Prayogi <rizkyden06@gmail.com>
2023-03-06 16:04:24 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/tugasbasicandroid/data/Artikel.kt
@@ -0,0 +1,7 @@
+package com.dicoding.tugasbasicandroid.data
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Artikel(val nama: String, val deskripsi: String, val gambar: String): Parcelable
